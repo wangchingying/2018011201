830dd077d4252e9d7b815f51f6dd0a90560cf5d2
totorowg <wangchingying@gmail.com>
2018-01-12 10:25:48 +0800

    抓JSON格式

--- app/src/main/java/com/cyw/a2018011201/MainActivity.java
+++ app/src/main/java/com/cyw/a2018011201/MainActivity.java
@@ -11,6 +11,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,11 +29,28 @@ public class MainActivity extends AppCompatActivity {
         //1.建立一個Queue
         RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
         //2.建立一個Request(1.網址2.資料抓成功了怎麼半3.資料抓失敗了怎麼半)
-        StringRequest request=new StringRequest("https://www.mobile01.com/rss/news.xml", new Response.Listener<String>() {
+        StringRequest request=new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json", new Response.Listener<String>() {
             @Override
             //response就是抓到的資料(此method不用寫執行緒)
             public void onResponse(String response) {
                 Log.d("Net",response);
+                try {
+                    //JSON的格式 , {}代表物件, []代表陣列
+                    JSONArray array=new JSONArray(response);
+                    for (int i=0;i<array.length();i++) {
+                        JSONObject obj = array.getJSONObject(i);
+                        String str1 = obj.getString("district");
+                        Log.d("Net", str1);
+                        String str2 = obj.getString("address");
+                        Log.d("Net", str2);
+                        String str3 = obj.getString("tel");
+                        Log.d("Net", str3);
+                        String str4 = obj.getString("opening_hours");
+                        Log.d("Net", str4);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
